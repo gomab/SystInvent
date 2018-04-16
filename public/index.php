a460960b74dd62802ff6384e23e4b35930fddccb
gomab <egoiomab@gamil.com>
2018-04-16 18:43:01 +0200

    Login Form

--- public/index.php
+++ public/index.php
@@ -12,11 +12,41 @@
  
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-  </body>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <div class="jumbotron">
-        <h1>Hello</h1>
+    <!-- Navbar -->
+    <?php include_once('./templates/header.php')  ?>
+    <!-- End Navbar -->
+
+    <br>
+    <div class="container">
+        <div class="card mx-auto" style="width: 18rem;">
+            <img class="card-img-top mx-auto" style="width:60%" src="./images/login.png" alt="Login icon">
+            <div class="card-body">
+                <h5 class="card-title"></h5> 
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-lock">&nbsp</i>Se connecter</button>
+                    <span><a href="#">S'enregistrer</a></span>
+                    </form>
+            </div>
+            <div class="card-footer">
+                <a href="#">Mot de passe oublié?</a>
+            </div>
+        </div>
     </div>
 </body>
 </html>
