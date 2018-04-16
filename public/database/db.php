05c483b880352cd2d974cd4d6a40da92cbc5ef9b
gomab <egoiomab@gamil.com>
2018-04-16 17:55:36 +0200

    Strucred Project and DB Connect

---
+++ public/database/db.php
@@ -0,0 +1,18 @@
+<?php
+    class Database{
+        private $con;
+
+        public function connect(){
+            include_once('constants.php');
+            $this->con = new Mysqli(HOST, USER, PASS, DB);
+
+            if($this->con){
+                echo "Connected";
+                return $this->con;
+            }
+            return "DATABASE_CONNECTION_FAIL";
+        }
+    }
+
+    $db = new database();
+    $db->connect();
